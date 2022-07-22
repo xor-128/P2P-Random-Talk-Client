534761f2e496bd1a4d65a02d52eba56360ceeda0
deniz yılmaz <deniz_ylmz_in@windowslive.com>
2022-07-23 02:47:26 +0300

    Add project files.

---
+++ P2P Random Talk Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P2PRandomTalkClient
+{
+    public static class ConsoleHandler
+    {
+        public static void SetTitle(string val)
+        {
+            Console.Title = val;
+        }
+        public static string Read()
+        {
+            return Console.ReadLine() ?? "";
+        }
+        public static void Write(string val, bool newline = false)
+        {
+            Console.Write(val + (newline ? Environment.NewLine : ""));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleHandler.SetTitle("P2P Random Talk Client");
+
+        }
+    }
+}
